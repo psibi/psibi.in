40215895e62d323d34a5e208ba925054f15376ee
Sibi <psibi2000@gmail.com>
2012-01-17 15:58:21 +0530

    Renders main page

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,1 +1,10 @@
+from django.http import HttpResponse
+from django.template import Context
+from django.template.loader import get_template
+
+def main_page(request):
+    template=get_template('index.html')
+    variables=Context({})
+    output = template.render(variables)
+    return HttpResponse(output)
 # Create your views here.
